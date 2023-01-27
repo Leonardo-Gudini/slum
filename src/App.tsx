8d814b22524ba88aa86beff2a1e50882ffa741ac
Leonardo Gudini <leonardo.gudini98@gmail.com>
2023-01-27 17:44:08 +0100

    Prova di verifica front-end SLUM

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export type Photo = {
+  "albumId": number,
+  "id": number,
+  "title": string,
+  "url": string,
+  "thumbnailUrl": string
+}
+
+export type Album = {
+  "userId": number,
+  "id": number,
+  "title": string
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [photos, setPhotos] = useState<Photo[] | null>(null);
+
+  const [albums, setAlbums] = useState<Album[] | null>(null);
+
+    useEffect(()=>{
+      axios.all([
+        axios.get('https://jsonplaceholder.typicode.com/photos'),
+        axios.get('https://jsonplaceholder.typicode.com/albums')
+      ])
+      .then(axios.spread((photos, albums) => {
+        setPhotos(photos.data)
+        setAlbums(albums.data)
+      }))
+    })
+
+    const changeResolution = (url: string)=>{
+      window.open(url)
+    }
+
+    ////////////
+
+    return(
+      <div>
+        {albums?.map((album, index) => {
+          return(
+            <div key={index}>
+              <h1 className='album-title'>{album.title}</h1>
+              {photos?.map((photo, index) => {
+                if (photo.albumId === album.id) {
+                  return (
+                      <div key={index}>
+                          <div className='container'>
+                            <div className='photo-item'>
+                              <img className='image' onClick={()=>changeResolution(photo.url)} src={photo.thumbnailUrl} alt={photo.title} />
+                              <ul>
+                                <li>Title: {photo.title}</li>
+                                <li>Album ID: {photo.albumId}</li>
+                                <li>ID: {photo.id}</li>
+                              </ul>
+                            </div>
+                          </div>
+                      </div>
+                  )
+                }
+                
+              })}
+            </div>
+          )
+        })}
+      </div>
+    )
+
+    // return(
+    //   <div>
+    //     {albums?.map((album, index) => {
+    //       return (
+    //         <div key={index}>
+    //           <h1 className='albumTitle'>{album.title}</h1>
+    //           {photos?.map((photo, index) => {
+    //             return (
+    //               <div className='d-flex flex-wrap' key={index}>
+    //                 <img className='image' onClick={()=>changeResolution(photo.url)} src={photo.thumbnailUrl} alt={photo.title} />
+    //               </div>
+    //             )
+    //           })}
+    //         </div>
+    //       )
+    //     })}
+    //   </div>
+    // )
+
+    
+
+    // return (
+    //     <div className="App">
+    //         {photos
+    //             ? photos.map((photo) => {
+    //                   return <div>
+    //                   <ul>
+    //                     <li>Photo Title: {photo.title}</li>
+    //                     <li>Album ID: {photo.albumId}</li>
+    //                     <li>ID: {photo.id}</li>
+    //                   </ul>
+    //                   <img onClick={()=>changeResolution(photo.url)} src={photo.thumbnailUrl} alt={photo.title}/>
+    //                 </div>;
+    //               })
+    //             : null}
+
+    //         {albums
+    //             ? albums.map((album) => {
+    //               return <div>
+    //                 <ul>
+    //                   <li>Album Title: {album.title}</li>
+    //                 </ul>
+    //               </div>
+    //             })
+    //           : null}
+    //     </div>
+    // );
 }
 
 export default App;
